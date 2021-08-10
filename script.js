f507a77da001842f2be62407e3031d81fcba7f9d
Tautvydas Žukauskas <tautvydaszuk@gmail.com>
2021-08-11 02:50:44 +0300

    add function to display books in browser window

--- script.js
+++ script.js
@@ -1,17 +1,55 @@
-let bookLibrary = [];
+const libraryContainer = document.querySelector("#booksContainer")
+const newBook = document.createElement("div")
+const bookTitle = document.createElement("p")
+const bookAuthor = document.createElement("p")
+const bookPages = document.createElement("p")
+const bookReadStatus = document.createElement("p")
+
+let bookLibrary = [ {
+  title: "test",
+  author: "test1",
+  pages: "234",
+  readStatus: "no"},
+  {
+    title: "Harry Potter",
+    author: "Rowling",
+    pages: "256",
+    readStatus: "yes"}
+];
 
 function book(title, author, pages, readStatus) {
   this.title = title
   this.author = author
   this.pages = pages
   this.readStatus = readStatus
-  }
+  };
 
 function addBookToLibrary() {
   let title = prompt("Book title?")
   let author = prompt("Who's the author")
   let pages = prompt("How many pages?")
   let readStatus = prompt("Have you read it?")
-  const newBook = new book(title, author, pages, readStatus)
-  bookLibrary.push(newBook)
+  const Book = new book(title, author, pages, readStatus)
+  bookLibrary.push(Book)
+};
+
+function Display() {
+  for (let i = 0; i < bookLibrary.length; i++) {
+    const newBook = document.createElement("div")
+    newBook.classList.add("book")
+    newBook.setAttribute("id", i)
+    const bookTitle = document.createElement("p")
+    const bookAuthor = document.createElement("p")
+    const bookPages = document.createElement("p")
+    const bookReadStatus = document.createElement("p")
+  libraryContainer.appendChild(newBook)
+  bookTitle.textContent = "Title: " + bookLibrary[i].title
+  newBook.appendChild(bookTitle)
+  bookAuthor.textContent = "Author: " + bookLibrary[i].author
+  newBook.appendChild(bookAuthor)
+  bookPages.textContent = "Pages: " + bookLibrary[i].pages
+  newBook.appendChild(bookPages)
+  bookReadStatus.textContent = "ReadStatus: " + bookLibrary[i].readStatus
+  newBook.appendChild(bookReadStatus)
+}
 }
